f5eb882f83e34e128e349b48837deb5c3dd0b55f
hyezo <hyezoprk@kakao.com>
2022-10-04 10:39:15 +0900

    🔖chore: added Vercel adaptio in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [react(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   output: "server",
+  adapter: vercel()
 });
